c968d363ad0af9822de188d0d33087f62813ac7f
Coleton Fischer <coleton@MacBookPro.attlocal.net>
2025-06-19 16:16:12 -0400

    Remove user prop from TopNav in dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default async function DashboardLayout({
     <div className="flex h-screen bg-background">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-        <TopNav user={user} />
+        <TopNav />
         <main className="flex-1 overflow-y-auto p-6">
           {children}
         </main>
